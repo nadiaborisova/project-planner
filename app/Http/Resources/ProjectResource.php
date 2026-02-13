b8e12b09cef2faf008b664d82eab337e0bd1c90c
Nadezhda <me4o_turist@abv.bg>
2026-02-13 20:36:18 +0200

    Resources added - User, Project, Task

---
+++ app/Http/Resources/ProjectResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProjectResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'status' => $this->status,
+            'tasks' => TaskResource::collection($this->whenLoaded('tasks')),
+            'stats' => [
+                'total_tasks' => $this->tasks_count,
+            ],
+        ];
+    }
+}
