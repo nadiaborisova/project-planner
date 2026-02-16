69037710a76f3fe2d24bd23e0aaecbb03f2fc48c
Nadezhda <me4o_turist@abv.bg>
2026-02-16 17:14:07 +0200

    User relationship in Activity model added

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -3,10 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Activity extends Model
 {
-    protected $fillable = ['user_id','type','subject_type','subject_id'];
+    protected $fillable = ['user_id', 'type', 'subject_type', 'subject_id'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 
     public function subject()
     {
